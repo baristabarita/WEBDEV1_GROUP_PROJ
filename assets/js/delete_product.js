6c4ce93ba7fda1a78d36814b49b83269ee008d17
makiweeb13 <mariamaekiskis13@gmail.com>
2023-04-19 12:30:43 +0800

    a

--- assets/js/delete_product.js
+++ assets/js/delete_product.js
@@ -1,12 +1,25 @@
+
+// document selectors
+
 const deleteBtn = document.querySelectorAll('.delete');
 const continueBtn = document.querySelector('.continue-btn');
 const cancelBtn = document.querySelector('.cancel-btn');
 const modalOverlay = document.querySelector('.modal-overlay');
+const contBtn = document.getElementById('cont-btn');
+
+// state declarations
+
+let product;
 
 deleteBtn.forEach(btn => btn.addEventListener('click', () => {
+    console.log(btn.parentElement)
     modalOverlay.classList.add('open-modal')
 }))
 
 cancelBtn.addEventListener('click', () => {
     modalOverlay.classList.remove('open-modal')
+})
+
+contBtn.addEventListener('click', () => {
+
 })
