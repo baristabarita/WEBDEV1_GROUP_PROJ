3c793df8c083db9f1e0e5ea8fefdeecab9f37949
makiweeb13 <mariamaekiskis13@gmail.com>
2023-05-01 10:24:54 +0800

    dd

--- assets/js/sales_chart.js
+++ assets/js/sales_chart.js
@@ -1,7 +1,7 @@
 const ctx = document.getElementById('myChart').getContext('2d');
 
-  new Chart(ctx, {
-    type: 'bar',
+new Chart(ctx, {
+  type: 'bar',
     data: {
       labels: ['Today', 'This Week', 'This Month', 'This Year'],
       datasets: [{
@@ -19,4 +19,35 @@ const ctx = document.getElementById('myChart').getContext('2d');
     options: {
       responsive: true,
     }
-  });
+});
+
+// Dummy percent value
+const percentValue = 75;
+
+// Create the doughnut chart
+const chart = new Chart(document.getElementById("total-sales-graph").getContext('2d'), {
+  type: 'doughnut',
+  data: {
+    labels: ['Sold', 'Remaining'],
+    datasets: [{
+      label: 'Sales',
+      backgroundColor: ['#28a745', '#dc3545'],
+      data: [percentValue, 100 - percentValue]
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    cutoutPercentage: 80,
+    legend: {
+      display: false
+    },
+    tooltips: {
+      callbacks: {
+        label: function(tooltipItem, data) {
+          return data.labels[tooltipItem.index] + ': ' + data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index] + '%';
+        }
+      }
+    }
+  }
+});
